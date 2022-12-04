cae5232e9aeee7784e29e55ea8df8daa92511bb9
demimao <demimao@users.noreply.github.com>
2022-12-04 04:02:58 +0000

    Prettified Code!

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,10 @@
 const config = {
-    "preset": "jest-puppeteer",
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-      "src/**/*"
-    ],
-    "coverageReporters": [
-      "text",
-      "lcov",
-      "cobertura"
-    ],
-    "setupFilesAfterEnv": [
-      "jest-puppeteer-istanbul/lib/setup"
-    ],
-    "reporters": [
-      "default",
-      "jest-puppeteer-istanbul/lib/reporter"
-    ],
-    "coverageDirectory": "coverage"
-  }
+  preset: "jest-puppeteer",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*"],
+  coverageReporters: ["text", "lcov", "cobertura"],
+  setupFilesAfterEnv: ["jest-puppeteer-istanbul/lib/setup"],
+  reporters: ["default", "jest-puppeteer-istanbul/lib/reporter"],
+  coverageDirectory: "coverage",
+};
 module.exports = config;
