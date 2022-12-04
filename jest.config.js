8b895f46639f1d01188984c51109a91cf791f83a
demimao <83380862+demimao@users.noreply.github.com>
2022-12-03 20:23:10 -0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
-const config = {
-  preset: "jest-puppeteer",
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*"],
-  coverageReporters: ["text", "lcov", "cobertura"],
-  setupFilesAfterEnv: ["jest-puppeteer-istanbul/lib/setup"],
-  reporters: ["default", "jest-puppeteer-istanbul/lib/reporter"],
-  coverageDirectory: "coverage",
-};
-module.exports = config;
+module.exports = {
+    launch: {
+      headless: false,
+      slowMo: 25
+    }
+  }
