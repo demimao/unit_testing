944d369ca2c4e4437cfb48a1445dcc834b2f79f7
demimao <83380862+demimao@users.noreply.github.com>
2022-12-03 20:00:50 -0800

    Update jest.puppeteer.config.js

--- jest.puppeteer.config.js
+++ jest.puppeteer.config.js
@@ -1,6 +1,26 @@
-module.exports = {
-  launch: {
-    headless: true,
-    slowMo: 1,
-  },
-};
+// get environment varable
+const ci = Boolean(process.env.CI || false);
+
+const baseOptions = {
+    server: {
+        command: 'npm run start',
+        port: 9999
+      }
+}
+
+const ciPipelineOptions = {
+    launch: {
+        executablePath: '/usr/bin/google-chrome-stable',
+        headless: true,
+        args: [
+          '--ignore-certificate-errors',
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-accelerated-2d-canvas',
+          '--disable-gpu'
+              ]
+    },
+    server: baseOptions.server
+}
+
+module.exports = ci ? ciPipelineOptions : baseOptions;
