0c7438a1d5a6a2e27d2283c8894b46823f2f365d
demimao <demimao@users.noreply.github.com>
2022-12-04 04:01:08 +0000

    Prettified Code!

--- jest.puppeteer.config.js
+++ jest.puppeteer.config.js
@@ -2,25 +2,25 @@
 const ci = Boolean(process.env.CI || false);
 
 const baseOptions = {
-    server: {
-        command: 'npm run start',
-        port: 9999
-      }
-}
+  server: {
+    command: "npm run start",
+    port: 9999,
+  },
+};
 
 const ciPipelineOptions = {
-    launch: {
-        executablePath: '/usr/bin/google-chrome-stable',
-        headless: true,
-        args: [
-          '--ignore-certificate-errors',
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-accelerated-2d-canvas',
-          '--disable-gpu'
-              ]
-    },
-    server: baseOptions.server
-}
+  launch: {
+    executablePath: "/usr/bin/google-chrome-stable",
+    headless: true,
+    args: [
+      "--ignore-certificate-errors",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-accelerated-2d-canvas",
+      "--disable-gpu",
+    ],
+  },
+  server: baseOptions.server,
+};
 
 module.exports = ci ? ciPipelineOptions : baseOptions;
